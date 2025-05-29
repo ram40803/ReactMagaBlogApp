d4fbd26a9e61c495e0cb3eb5430602cf3500a52a
Ram Patidar <rampatidar40803@gmail.com>
2025-05-29 19:20:11 +0530

    implement loading logic in app file

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,27 @@
 import { useState } from 'react'
 import './App.css'
+import { useDispatch } from 'react-redux';
+import authService from './appwrite/auth';
+import {login, logout} from "./store/authSlice"
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
 
-  return (
-    <>
-      <h>Megablog</h>
-    </>
-  )
+  useEffect(() => {
+    authService.getCurrentUser()
+    .then((userData) => {
+      if(userData) {
+        dispatch(login({userData}))
+      } else {
+        dispatch(logout());
+      }
+    })
+    .finally(() => setLoading(false))
+  }, [])
+
+  return !loading ? (<></>) : (<></>);
+    
 }
 
 export default App
