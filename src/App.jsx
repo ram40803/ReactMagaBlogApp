782e4b07cf7c96bb0415098d69ad1a853a33e4a0
Ram Patidar <rampatidar40803@gmail.com>
2025-06-01 17:52:56 +0530

    some changies are done in few files

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './App.css'
 import { useDispatch } from 'react-redux';
 import authService from './appwrite/auth';
-import {login, logout} from "./store/authSlice"
+import {login, logout} from "./store/authSlice";
+import {Header, Footer} from './components/index';
 
 function App() {
   const [loading, setLoading] = useState(true);
@@ -18,9 +19,19 @@ function App() {
       }
     })
     .finally(() => setLoading(false))
-  }, [])
+  }, []);
 
-  return !loading ? (<></>) : (<></>);
+  return !loading ? (
+    <div className='min-h-screen min-w-screen flex flex-wrap content-between bg-gray-400'>
+      <div className='w-full block'>
+        <Header />
+        <main>
+          {/* <Outlet /> */}
+        </main>
+        <Footer />
+      </div>
+    </div>
+  ) : (<></>);
     
 }
 
